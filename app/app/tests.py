b2a070bc3447eba0d083a4815dc4a3e75b2023bf
Kevin Wood <kevin.wood75@gmail.com>
2019-05-25 13:37:40 -0400

    added code

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,9 +4,11 @@ from app.calc import add, subtract
 
 class CalcTests(TestCase):
 
+
     def test_add_numbers(self):
         """Test that two numbers are added togethor"""
         self.assertEqual(add(3, 8), 11)
+
 
     def test_subtract_numbers(self):
         """Test that two numbers are added togethor"""
