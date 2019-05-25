dba250688ac2f5afafec62f1e5f439e8a9c92629
Kevin Wood <kevin.wood75@gmail.com>
2019-05-25 13:23:27 -0400

    added code

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,3 +3,7 @@
 def add(x, y):
     """Add two numbers togethor"""
     return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return value"""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -7,3 +7,7 @@ class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that two numbers are added togethor"""
         self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test that two numbers are added togethor"""
+        self.assertEqual(subtract(5, 11), 6)
