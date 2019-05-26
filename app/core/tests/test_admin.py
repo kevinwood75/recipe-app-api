02f51d015d9930b98dfc60bd2ea9fabd64f481b1
Kevin Wood <kevin.wood75@gmail.com>
2019-05-26 14:41:06 -0400

    add code

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,5 +7,20 @@ class AdminSiteTests(TestCase):
     def setup(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            
+            email='admin@woodez.org',
+            password='password123'
         )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@woodez.org',
+            password='password123',
+            name='Test user full name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
