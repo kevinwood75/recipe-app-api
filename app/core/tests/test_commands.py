230da33df9ce3758f6870c76e4eced8b91bc480e
Kevin Wood <kevin.wood75@gmail.com>
2019-05-28 18:31:10 -0400

    added code

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -5,10 +5,11 @@ from django.db.utils import OperationalError
 from django.test import TestCase
 
 
-class CommandTests(TestCase):
+class CommandsTestCase(TestCase):
    
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
+        
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.return_value = True
             call_command('wait_for_db')
