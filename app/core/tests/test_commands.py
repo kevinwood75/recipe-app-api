6d19c57af3934bb96f0196e692e6168f663a164e
Kevin Wood <kevin.wood75@gmail.com>
2019-05-28 18:37:51 -0400

    added code

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -15,9 +15,10 @@ class CommandsTestCase(TestCase):
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
 
-    @patch('time.sleep', return_value=True)
-    def test_wait_for_db(self):
+    @patch('time.sleep', return_value=None)
+    def test_wait_for_db(self, ts):
         """Test waiting for db"""
+
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.side_effect = [OpertionlError] * 5 + [True]
             call_command('wait_for_db')
