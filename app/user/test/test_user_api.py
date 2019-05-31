253890a9e31c35427d50f696bcce67e9a7672e79
Kevin Wood <kevin.wood75@gmail.com>
2019-05-30 22:41:11 -0400

    added code

--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -108,8 +108,8 @@ class PrivateUserApiTestsTestCase(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
-            'name': self.user.name
-            'email': self.user.email
+            'name': self.user.name,
+            'email': self.user.email,
         })
     def test_post_me_not_allowed(self):
         """test that POST is not allowed on the me url"""
